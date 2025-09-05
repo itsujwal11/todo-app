8736ce43d05db5a5c71d90edfb3180923aad00a7
itsujwal11 <haddyref@gmail.com>
2025-09-05 22:22:08 +0545

    ereg

--- app.js
+++ app.js
@@ -2,16 +2,39 @@ const input = document.getElementById("taskInput");
 const button = document.getElementById("addBtn");
 const list = document.getElementById("taskList");
 
-button.onclick(function(){
+button.onclick= function(){
 
 const taskText = input.value;
+if(taskText ==="") return;
 
 const li = document.createElement("li");
 li.textContent = taskText;
 
+
+const del = document.createElement("button");
+del.textContent = "Delete"
+del.style.marginLeft = "8px";
+
+li.appendChild(del);
 list.appendChild(li);
 
 input.value = "";
 
 
-})
+}
+
+list.addEventListener("click", function(e){
+if(e.target.tagName === "BUTTON"){
+    e.target.parentElement.remove();
+}else if(e.target.tagName === "LI"){
+    e.target.classList.toggle("done")
+}
+
+});
+
+input.addEventListener("keydown" ,function(e){
+if(e.key === "Enter"){
+    button.click();
+}
+
+});
